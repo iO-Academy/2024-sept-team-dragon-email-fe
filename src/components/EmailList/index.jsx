75c7d2ed035fda5f1b8a4e1801bcec63f1c39c53
Anna Saunders <annacfsaunders@hotmail.com>
2024-10-28 17:05:54 +0000

    setting up fetch request

--- src/components/EmailList/index.jsx
+++ src/components/EmailList/index.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import EmailPreview from "../EmailPreview";
 
 function EmailList() {
+
+    const [email, setEmail] = useState([])
+
+function getEmailData () {
+    fetch("https://email-client-api.dev.io-academy.uk/emails")
+        .then(res => res.json())
+        .then(emailData => {
+
+            console.log(emailData.data)
+//            console.log(data.0.name)
+//            setEmailName(data.name)
+//            setEmailSubject(data.subject)
+//            setEmailBody(data.body)
+        })
+}
+
+useEffect(getEmailData, [])
+
     return (
         <div className="border-b-2">
             <EmailPreview />
@@ -8,5 +27,6 @@ function EmailList() {
             <EmailPreview />
         </div>
     )
+
 }
 export default EmailList;
