1cbf070bd0f39fbc49c31f54000726c224333bf3
Robbie Hollis <robbiehollis2004@gmail.com>
2024-10-28 15:29:40 +0000

    Added Navbar and MenuButton component

---
+++ src/components/MenuButton/index.jsx
@@ -0,0 +1,12 @@
+
+function MenuButton (){
+       return (
+         <nav>
+            <button className="border-4 p-1 rounded-md border-white font-normal
+            hover:bg-slate-400">MENU</button>
+        </nav>
+       )
+ }
+
+
+ export default MenuButton
