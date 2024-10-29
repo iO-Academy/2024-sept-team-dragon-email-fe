3add1a081bbc52c7f1b164ffe35b63ac12e1ffd3
Ian Blenkinsop <blenka2@gmail.com>
2024-10-29 16:36:24 +0000

    added Route pages for emailList as 'home page'/default and added viewEmail page for viewing single mail messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import EmailList from "./components/EmailList"
 import Nav from "./components/Nav"
+import ViewEmail from "./pages/ViewEmail"
 
 function App() {
   return (
     <BrowserRouter>
       <div>
           <Nav />
-          <EmailList /> 
+          
       </div>
       <Routes>
-        
+        <Route path="/" element={<EmailList /> } /> 
+        <Route path="/viewemail" element={ <ViewEmail />} />
+      
       </Routes>
     </BrowserRouter>
   )
