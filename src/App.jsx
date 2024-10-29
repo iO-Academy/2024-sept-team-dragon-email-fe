5c1955a533cb59e589e8095ff24c3e677625e33d
syed faheema <asyedfaheema@gmail.com>
2024-10-29 16:24:17 +0000

    Created Homepage and Composepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import EmailList from "./components/EmailList"
 import Nav from "./components/Nav"
 import ComposePage from "./pages/ComposePage"
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Nav />
       <Routes>
+      <Route path="/"element={<HomePage/>}/>
       <Route path="/compose"element={<ComposePage/>}/>
+      
      
       </Routes>
-      <Nav />
-      <EmailList /> 
+     
+     
       </BrowserRouter>
     </div>
   )
