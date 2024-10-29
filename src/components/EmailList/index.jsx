6aadefb9d12ac839888cb10f7b4466cf3314ea9a
Anna Saunders <annacfsaunders@hotmail.com>
2024-10-29 10:06:13 +0000

    got fetch request working to display each email

--- src/components/EmailList/index.jsx
+++ src/components/EmailList/index.jsx
@@ -3,29 +3,36 @@ import EmailPreview from "../EmailPreview";
 
 function EmailList() {
 
-    const [email, setEmail] = useState([])
+    const [emails, setEmails] = useState([])
 
 function getEmailData () {
     fetch("https://email-client-api.dev.io-academy.uk/emails")
         .then(res => res.json())
         .then(emailData => {
-
+            setEmails (emailData.data)
             console.log(emailData.data)
-//            console.log(data.0.name)
-//            setEmailName(data.name)
-//            setEmailSubject(data.subject)
-//            setEmailBody(data.body)
+//            console.log(emailData.data[0].name)
+//            console.log(emailData.data[].name)
+//            setEmailName(data[].name)
+//            setEmailSubject(data[].subject)
+//            setEmailBody(data[].body)
         })
+
 }
 
+    
 useEffect(getEmailData, [])
 
     return (
+        <>
         <div className="border-b-2">
-            <EmailPreview />
-            <EmailPreview />
-            <EmailPreview />
+        {emails.map(email => <EmailPreview  emailName= {email.name}
+                                            emailSubject={email.subject}
+                                            emailBody={email.body}
+                                            emailDate={email.date_created}/>)}
+
         </div>
+        </>
     )
 
 }
