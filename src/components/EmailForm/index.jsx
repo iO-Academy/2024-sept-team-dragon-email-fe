e6151d3f488079aeb9f4d130e231f812486de132
syed faheema <asyedfaheema@gmail.com>
2024-10-30 12:17:13 +0000

    Added email,subject,message into the email form

--- src/components/EmailForm/index.jsx
+++ src/components/EmailForm/index.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
+
 function EmailForm(){
+    const [email,setEmail]=useState('')
+    const [subject,setSubject]=useState('')
+    const [message,setMessage]=useState('')
+    const handleSubmit=(e)=> {
+        e.preventDefault()
+        console.log(email)
+        console.log(subject)
+        console.log(message)
+        
+    }
     return(
-        <form className="flex flex-col py-4 px-8 gap-4">
+        <form onSubmit={handleSubmit}
+        className="flex flex-col py-4 px-8 gap-4">
             <label htmlFor="email" />
             <input
             type="email"
             id="email"
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
             placeholder="To"
             className="border-2 rounded border-gray-200 p-2"
             />
@@ -13,6 +28,8 @@ function EmailForm(){
             <input
             type="text"
             id="subject"
+            value={subject}
+            onChange={(e)=> setSubject(e.target.value)}
             placeholder="Subject"
             className="border-2 rounded border-gray-200 p-2"
             />
@@ -22,9 +39,14 @@ function EmailForm(){
             name="message"
             rows="20"
             cols="30"
+            value={message}
+            onChange={(e)=> setMessage(e.target.value)}
             placeholder="Type your message here"
             className="border-2 rounded border-gray-200 p-2"
-            />  
+            /> 
+            <input 
+            type="submit"
+            />
         </form> 
     )
 }
