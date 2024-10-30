15581383f31066cf14531fdee883e6a756953d10
emsmarshall <mikemarshall1988@gmail.com>
2024-10-30 14:24:00 +0000

    styling unreadcount

--- src/components/DropdownButton/index.jsx
+++ src/components/DropdownButton/index.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 function DropdownButton ({name, count, link}) {
     return (
-        <Link to={link} className="border-b hover:bg-slate-500 p-5">
-            <span>{name}</span> <span>{count}</span>
+        <Link to={link} className="border-b hover:bg-slate-500 p-5 flex justify-between gap-12">
+            <span>{name}</span> <span className="p-1 py-1.5 text-xs bg-cyan-700 rounded-full">{count}</span>
         </Link>
     )
 }
