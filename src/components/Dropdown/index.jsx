55bb234a8213f1348dc58c6365459e0be73c9dd9
Charlie Bird <cbvvrd@gmail.com>
2024-10-31 12:32:46 +0000

    merging dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -9,15 +9,8 @@ function Dropdown({unreadCount}) {
                 <DropdownButton name="New Email"
                                 link="/compose"/>
                 <DropdownButton name="Inbox"
-<<<<<<< HEAD
                                 count={unreadCount}
                                 link="/"/>                
-=======
-                                count={unreadEmailCount}
-                    link="/" />
-                <DropdownButton name="Deleted"
-                    link="/deleted" />
->>>>>>> eb471b3b8b22e3d656eb016625619c33c16a2f79
             </div>
         </div>
     )
